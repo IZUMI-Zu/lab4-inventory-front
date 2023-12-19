6437fdeff895b50d87c1c85726937c694413d656
BSS ZU <274620705z@gmail.com>
2023-12-19 16:05:41 +0800

    Update SerialConfigure component labels and styles

--- src/components/developer/SerialConfigure.tsx
+++ src/components/developer/SerialConfigure.tsx
@@ -8,8 +8,8 @@ export const SerialConfigure = () => {
         <>
             <div className="grid grid-cols-4 min-w-full m-0">
                 <Input
-                    label="Current Port"
-                    value={"COM3"}
+                    label="Current Port State"
+                    value={portState}
                     isReadOnly
                     className="col-start-1 col-end-3 max-w-x p-6 pt-2 ml-1"
                 >
--- src/components/developer/content.tsx
+++ src/components/developer/content.tsx
@@ -13,7 +13,7 @@ export const Content = () => {
                 </div>
                 <div className="gap-2">
                     <Card className=" min-w-full mt-5">
-                        <h3 className="text-2xl font-semibold p-3 pl-8">Serial Configure</h3>
+                        <h3 className="text-2xl font-semibold p-3 pt-5 pl-8">Serial Configure</h3>
                         <SerialConfigure />
                     </Card>
                     <Card className="grid grid-cols-3 min-w-full mt-3">
