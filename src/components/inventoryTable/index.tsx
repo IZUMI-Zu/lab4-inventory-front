48cf13baaacaa6d44d5f4dcbd74563305705ee6d
BSS ZU <274620705z@gmail.com>
2023-12-19 19:55:09 +0800

    Update inventoryTable component styling

--- src/components/inventoryTable/index.tsx
+++ src/components/inventoryTable/index.tsx
@@ -3,7 +3,7 @@ import TableWrapper from "../table/table"
 
 export const ITable = () => {
   return (
-    <div className="mx-auto w-full flex flex-col gap-4 p-12">
+    <div className="mx-auto w-full flex flex-col gap-4 p-12 pb-10">
 
       <h3 className="text-xl font-semibold">All Inventories</h3>
       <div className="max-w-[95rem] mx-auto w-full">
